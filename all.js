028405310d9e588d127a3acb1a30dc5866ea24a6
navyayc <navyayc4@gmail.com>
2017-04-25 11:38:56 -0500

    Adding files to github

---
+++ all.js
@@ -0,0 +1,8 @@
+var first=require('./add');
+var second=require('./mul');
+var third=require('./sub');
+var fourth=require('./div');
+console.log(first.add(11,12));
+console.log(second.mul(11,12));
+console.log(third.sub(11,12));
+console.log(fourth.div(11,12));
